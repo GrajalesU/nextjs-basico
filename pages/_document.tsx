feae8cc332fbef6ea4cdfb6da20cccd90e045926
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-14 16:36:22 -0500

    Add document extends

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/*favicon*/}
+        {/*web font*/}
+        {/*stylesheet*/}
+        {/*scripts/js*/}
+      </Head>
+      <body className="my-body-class">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
