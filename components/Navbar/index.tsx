d31dbb8020021ce41586c896d61d7f46516cb80f
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-14 19:54:50 -0500

    Add current path style

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
           }`}
         >
           <Link href="/about">
-            <HeaderElement src="/images/basket.png" text="Canasta" />
+            <HeaderElement src="/images/basket.png" text="Canasta (0)" />
           </Link>
         </li>
       </ul>
