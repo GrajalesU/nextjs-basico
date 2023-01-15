6726eca339695a730a6bffc49f120047bb7fda82
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-14 19:53:29 -0500

    Add current path style

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,11 +1,17 @@
 import HeaderElement from "@components/HeaderElement";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "./styles.module.css";
 export default function Navbar() {
+  const { pathname } = useRouter();
   return (
     <nav className={styles.nav}>
       <ul className={styles.navList}>
-        <li className={`${styles.navElement} ${styles.selected}`}>
+        <li
+          className={`${styles.navElement} ${
+            pathname === "/" && styles.selected
+          }`}
+        >
           <Link
             href="/"
             style={{
@@ -15,7 +21,11 @@ export default function Navbar() {
             <HeaderElement src="/logo/aguacatesMor32.png" text="AguacateMor" />
           </Link>
         </li>
-        <li className={styles.navElement}>
+        <li
+          className={`${styles.navElement} ${
+            pathname === "/about" && styles.selected
+          }`}
+        >
           <Link href="/about">
             <HeaderElement src="/images/basket.png" text="Canasta" />
           </Link>
