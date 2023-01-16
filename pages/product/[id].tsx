0bf9f3a466ebe9ef9afc973687cce9955461b467
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-16 15:15:03 -0500

    Change SSR to  dynamic SSG

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,24 +1,39 @@
 import ProductAbout from "@components/ProductAbout";
 import ProductContent from "@components/ProductContent";
+import { GetStaticProps } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
-export default function ProductDetail() {
-  const router = useRouter();
-  const { id } = router.query;
-  const [product, setProduct] = useState<TProduct>();
+export const getStaticPaths = async () => {
+  const res = await fetch("https://aguacate-mor.vercel.app/api/avo");
+  const data: TAPIAvoResponse = await res.json();
+  const { data: productList } = data;
+  const paths = productList.map(({ id }) => ({
+    params: {
+      id,
+    },
+  }));
 
-  useEffect(() => {
-    window
-      .fetch(`/api/avo/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setProduct(data);
-      });
-  }, []);
+  return {
+    paths,
+    fallback: false
+  };
+};
 
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params?.id as string;
+  const res = await fetch(`https://aguacate-mor.vercel.app/api/avo/${id}`);
+
+  const product: TAPIAVODetailResponse = await res.json();
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
+
+export default function ProductDetail({ product }: { product: TProduct }) {
   return (
     <>
       <Head>
