1987a8c42c364f3371a0b9321d83db92011d8ab8
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-16 18:06:29 -0500

    Add rewrite

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   images: {
     unoptimized: false,
   },
+  rewrites: async function(){
+    return [
+        {
+            source: "/avocado/:path*",
+            destination: "/product/:path*"
+        }
+    ]
+  }
 };
 
 module.exports = nextConfig;
